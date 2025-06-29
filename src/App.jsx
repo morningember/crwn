f3c5367ba01fe9087584d8931c77d15ac0e45010
Nii Darko Dodoo <vergilniidodoo@gmail.com>
2025-06-29 00:27:30 +0000

    Home page created with css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <div className="">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/HomePage.css";
+import Directory from "../components/Directory";
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
